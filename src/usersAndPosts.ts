4450786999a608db4545ee298801c4f11fe88177
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2026-08-20 07:02:01 +0000

    Moved tests outside of src folder.

--- src/usersAndPosts.ts
+++ src/usersAndPosts.ts
@@ -4,14 +4,9 @@ import { readFile } from 'node:fs/promises';
 import { type Post } from './types/Post.js';
 import { type User } from './types/User.js';
 
-
-// these variables specify the relative paths to the JSON files:
-const usersFile = new URL('../data/users.json', import.meta.url);
-const postsFile = new URL('../data/posts.json', import.meta.url);
-
 // both files are read and parsed, and their results stored in variables:
-const users: User[] = JSON.parse(await readFile(usersFile, 'utf8'));
-const posts: Post[] = JSON.parse(await readFile(postsFile, 'utf8'));
+const posts: Post[] = JSON.parse(await readFile('data/posts.json', 'utf8'));
+const users: User[] = JSON.parse(await readFile('data/users.json', 'utf8'));
 
-// Part 2: write your logic for printing the names of the users and the titles 
+// Part 2: write your logic for printing the names of the users and the titles
 // of their own posts
